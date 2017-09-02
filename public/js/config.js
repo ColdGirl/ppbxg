856baa2f86fb20af5447f63e463717d36582b96f
ColdGirl <qin547464@icloud.com>
2017-09-03 00:33:22 +0800

    完成个人中心页面展示功能

--- public/js/config.js
+++ public/js/config.js
@@ -14,7 +14,8 @@ require.config({
 		datepicker : 'bootstrap-datepicker/js/bootstrap-datepicker',
 		language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
 		validate : 'validate/jquery-validate.min',
-		form: 'jquery-form/jquery.form'
+		form: 'jquery-form/jquery.form',
+		settings : '../js/settings'
 	},
 	/*bootstrap不是标准的模块 需要转化成标准的模块*/
 	shim :{
