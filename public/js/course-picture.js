b5c6f8a4278997660b1b5bdbbb8f526caeacc606
ColdGirl <qin547464@icloud.com>
2017-09-04 23:50:06 +0800

    展示课程封面信息

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,18 @@
 define(['jquery','template','util'],function($,template,util){
 	//设置导航菜单选中
 	util.setMenu('/course/add');
+
+	//获取课程id
+	var csId = util.queryString('cs_id');
+	//根据id查询课程的封面信息
+	$.ajax({
+		type: 'get',
+		url: '/api/course/picture',
+		data: {cs_id:csId},
+		dataType: 'json',
+		success: function(data){
+			var html = template('pictureTpl',data.result);
+			$('#pictureInfo').html(html);
+		}
+	});
 });
