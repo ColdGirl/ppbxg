5c679da1c487a2025e23f7d33083d3982ba19ea9
ColdGirl <qin547464@icloud.com>
2017-09-05 00:11:32 +0800

    完成封面上传功能

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','uploadify'],function($,template,util){
 	//设置导航菜单选中
 	util.setMenu('/course/add');
 
@@ -13,6 +13,22 @@ define(['jquery','template','util'],function($,template,util){
 		success: function(data){
 			var html = template('pictureTpl',data.result);
 			$('#pictureInfo').html(html);
+			//处理封面上传
+			$('#upfile').uploadify({
+				width: 80,
+				height: 'auto',
+				buttonText: '选择好图',
+				itemTemplate: '<span></span>',
+				buttonClass: 'btn btn-success btn-sm',
+				swf: '/public/assets/uploadify/uploadify.swf',
+				uploader: '/api/uploader/cover',
+				fileObjName: 'cs_cover_original',//上传的是文件的内容 大 不单单是字符串
+				formData: {cs_id: csId},//额外的参数 纯数据 字符串
+				onUploadSuccess: function(f,data){
+					data = JSON.parse(data);
+					$('.preview img').attr('src',data.result.path);
+				}
+			})
 		}
 	});
 });
